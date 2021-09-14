44ccf641676f1b0a699e24ef71a0b588360281e2
qinwen <qinwen@dian.so>
2021-09-14 09:54:20 +0800

    feat: add reflect annotation

---
+++ src/main/java/basis/annotations/AnnotationTest.java
@@ -0,0 +1,22 @@
+package basis.annotations;
+
+import java.lang.reflect.Field;
+
+/**
+ * @Author qinwen
+ * @Date 2021/9/14 8:27 上午
+ */
+public class AnnotationTest {
+    public static void main(String[] args) throws NoSuchFieldException {
+        // 获取类上的注解
+        // Annotation 'MyAnnotation.class' is not retained for reflective access
+        Class<Demo> clazz = Demo.class;
+        MyAnnotation annotationOnClass = clazz.getAnnotation(MyAnnotation.class);
+        System.out.println(annotationOnClass.getValue());
+
+        // 获取成员变量上的注解
+        Field field = clazz.getField("name");
+        MyAnnotation annotationOnField = field.getAnnotation(MyAnnotation.class);
+        System.out.println(annotationOnField);
+    }
+}
