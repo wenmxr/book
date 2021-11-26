ae312e62efeb365c55480e259832d2175d0f5189
qinwen <qinwen@dian.so>
2021-11-26 11:29:25 +0800

    feat: 锁

---
+++ src/main/java/basis/multithreading/ThreadDemo4.java
@@ -0,0 +1,41 @@
+package basis.multithreading;
+
+/**
+ * @Author qinwen
+ * @Date 2021/11/26 1:59 上午
+ */
+public class ThreadDemo4 {
+    /**
+     * 同步与非同步方式是否可以同时调用
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        ThreadDemo4 t = new ThreadDemo4();
+
+        new Thread(t::m1,"t1").start();
+        new Thread(t::m2,"t2").start();
+    }
+
+    /**
+     * 锁对象为this
+     */
+    public synchronized void m1() {
+        System.out.println(Thread.currentThread().getName() + " m1 start");
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName() + " m1 end");
+    }
+
+    public void m2() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName() + "m2 start");
+    }
+}
